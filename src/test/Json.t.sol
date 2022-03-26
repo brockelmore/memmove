2314ac9dc1549da4fb43521db443f058bc7dfe2a
Brock <brock.elmore@gmail.com>
2022-03-26 18:28:39 -0400

    test fix

--- src/test/Json.t.sol
+++ src/test/Json.t.sol
@@ -20,39 +20,39 @@ contract JsonTest is DSTest {
         // }
 
         Json json = JsonLib.newJson(2);
-        Array arr = ArrayLib.newArray(1);
-        // Array arr2 = ArrayLib.newArray(5);
-        // for (uint256 i; i < 5; i++) {
-        //     arr.unsafe_push(i);
-        // }
+        Array arr = ArrayLib.newArray(4);
+        Array arr2 = ArrayLib.newArray(5);
+        for (uint256 i; i < 5; i++) {
+            arr.push(i);
+        }
 
-        // for (uint256 i; i < 5; i++) {
-        //     arr2.unsafe_push(i);
-        // }
+        for (uint256 i; i < 5; i++) {
+            arr2.unsafe_push(i);
+        }
 
         json.insert("test1", arr);
-        // json.insert("test2", arr2);
+        json.insert("test2", arr2);
 
         string memory name = "name";
         json.insert("property",  name);
         // emit log_named_bytes32("name ptr", pointer(name));
         
-        // (, Array theArr) = json.getArray("test1");
-        // (, Array theArr2) = json.getArray("test2");
+        (, Array theArr) = json.getArray("test1");
+        (, Array theArr2) = json.getArray("test2");
         (, string memory prop) = json.getString("property");
 
-        // emit log_named_bytes32("prop ptr", pointer(prop));
-        // assertEq(prop, "name");
+        emit log_named_bytes32("prop ptr", pointer(prop));
+        assertEq(prop, "name");
         
-        // uint256 l1 = theArr.length();
-        // for (uint256 i;  i < l1; i++) {
-        //     assertEq(theArr.unsafe_get(i), i);
-        // }
+        uint256 l1 = theArr.length();
+        for (uint256 i;  i < l1; i++) {
+            assertEq(theArr.unsafe_get(i), i);
+        }
 
-        // uint256 l2 = theArr2.length();
-        // for (uint256 i;  i < l2; i++) {
-        //     assertEq(theArr2.unsafe_get(i), i);
-        // }
+        uint256 l2 = theArr2.length();
+        for (uint256 i;  i < l2; i++) {
+            assertEq(theArr2.unsafe_get(i), i);
+        }
     }
 
     function pointer(string memory a) internal view returns(bytes32 ptr) {
